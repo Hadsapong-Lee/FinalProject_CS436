0d85b28de9ecf9f2f4b2a9db68fe96d3a34724a1
HADSAPONG LEE <hadsapong.lee@bu.ac.th>
2024-11-18 15:41:59 +0700

    SentMail's now working

--- Pages/SentMail.cshtml.cs
+++ Pages/SentMail.cshtml.cs
@@ -1,12 +1,68 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 
 namespace FinalProject.Pages
 {
     public class SentMailModel : PageModel
     {
+        public List<EmailInfo> SentEmails = new List<EmailInfo>();
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public SentMailModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
+            try
+            {
+                string connectionString = "Server=tcp:datapj.database.windows.net,1433;Initial Catalog=datapj;Persist Security Info=False;User ID=fproject;Password=Final12Proj3ct;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string username = "";
+                    if (User.Identity.Name == null)
+                    {
+                        username = "";
+                    }
+                    else
+                    {
+                        username = User.Identity.Name;
+                    }
+
+                    Console.WriteLine(username);
+
+                    String sql = $"SELECT * FROM emails WHERE emailsender=\'{username}\' ORDER BY emaildate DESC";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                EmailInfo emailInfo = new EmailInfo();
+                                emailInfo.EmailID = "" + reader.GetInt32(0);
+                                emailInfo.EmailSubject = reader.GetString(1);
+                                emailInfo.EmailMessage = reader.GetString(2);
+                                emailInfo.EmailDate = reader.GetDateTime(3).ToString("dddd, dd MMMM yyyy HH:mm tt");
+                                emailInfo.EmailIsRead = "" + reader.GetInt32(4);
+                                emailInfo.EmailSender = reader.GetString(5);
+                                emailInfo.EmailReceiver = reader.GetString(6);
+
+                                SentEmails.Add(emailInfo);
+                            }
+                        }
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
+
 }
