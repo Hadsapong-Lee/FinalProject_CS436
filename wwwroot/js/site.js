ab5e7da11dff6284f0a49049af019367c7983bbc
HADSAPONG LEE <hadsapong.lee@bu.ac.th>
2024-11-18 14:39:32 +0700

    1

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const listItems = document.querySelectorAll('.wrapper .sidebar ul li');
+
+// Add click event listener to each item
+listItems.forEach(item => {
+    item.addEventListener('click', function () {
+        // Remove 'active' class from all items
+        listItems.forEach(li => li.classList.remove('active'));
+
+        // Add 'active' class to the clicked item
+        this.classList.add('active');
+    });
+});
