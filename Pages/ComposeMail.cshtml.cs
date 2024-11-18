73caa27cfdd61e4726e9fd459c3eb04f30d17ce0
HADSAPONG LEE <hadsapong.lee@bu.ac.th>
2024-11-18 15:33:24 +0700

    Before  พังing

--- Pages/ComposeMail.cshtml.cs
+++ Pages/ComposeMail.cshtml.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.SqlClient;
 
-namespace FinalProject.Pages.MailSystem
+namespace FinalProject.Pages
 {
     public class ComposeMailModel : PageModel
     {
