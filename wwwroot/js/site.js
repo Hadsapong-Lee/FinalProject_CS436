1248cc28e2af95db8ad68488125f7f1d091d0bab
Chontara <chontara.sang@bumail.net>
2024-11-19 17:01:17 +0700

    Last 17:00

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,33 +3,21 @@
 
 // Write your JavaScript code.
 
-const listItems = document.querySelectorAll('.wrapper .sidebar ul li');
 
-// Add click event listener to each item
-listItems.forEach(item => {
-    item.addEventListener('click', function () {
-        // Remove 'active' class from all items
-        listItems.forEach(li => li.classList.remove('active'));
 
-        // Add 'active' class to the clicked item
-        this.classList.add('active');
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuItems = document.querySelectorAll('.wrapper .sidebar ul li');
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', function () {
+            // Remove "active" class from all items
+            menuItems.forEach(i => i.classList.remove('active'));
+
+            // Add "active" class to the clicked item
+            this.classList.add('active');
+        });
     });
 });
-
-function setActive(element) {
-    // Remove "active" class from all list items
-    const items = document.querySelectorAll('.wrapper .sidebar ul li');
-    items.forEach(item => item.classList.remove('active'));
-
-    // Add "active" class to the clicked item
-    element.classList.add('active');
-}
-
-function setActive(element) {
-    // Remove "active" class from all list items
-    const items = document.querySelectorAll('.wrapper .sidebar ul li');
-    items.forEach(item => item.classList.remove('active'));
-
-    // Add "active" class to the clicked item
-    element.classList.add('active');
-}
