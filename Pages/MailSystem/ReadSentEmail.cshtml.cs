cedd83bb8807e37d0d8e42719c2893a476ddf4cf
WaterMedicine <135619627+WaterMedicine@users.noreply.github.com>
2024-11-20 23:13:27 +0700

    Done JingJing2

---
+++ Pages/MailSystem/ReadSentEmail.cshtml.cs
@@ -0,0 +1,56 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+
+namespace FinalProject.Pages
+{
+    public class ReadSentEmailModel : PageModel
+    {
+        public String EmailID { get; set; }
+        public String EmailSubject { get; set; }
+        public String EmailMessage { get; set; }
+        public String EmailDate { get; set; }
+        public String EmailIsRead { get; set; }
+        public String EmailSender { get; set; }
+        public String EmailReceiver { get; set; }
+
+        public IActionResult OnGet(string emailId)
+        {
+            try
+            {
+                String connectionString = "Server=tcp:datapj.database.windows.net,1433;Initial Catalog=datapj;Persist Security Info=False;User ID=fproject;Password=Final12Proj3ct;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    String sql = $"SELECT * FROM emails WHERE emailid = '{emailId}'";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                EmailID = reader.GetInt32(0).ToString();
+                                EmailSubject = reader.GetString(1);
+                                EmailMessage = reader.GetString(2);
+                                EmailDate = reader.GetDateTime(3).ToString("dddd, dd MMMM yyyy HH:mm tt");
+                                EmailIsRead = "" + reader.GetInt32(4);
+                                EmailSender = reader.GetString(5);
+                                EmailReceiver = reader.GetString(6);
+
+                                return Page();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
